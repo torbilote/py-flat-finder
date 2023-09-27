7a52c60a828a88bf0780e985e44896c2703b2ca9
Baarosh <45597075+Baarosh@users.noreply.github.com>
2023-09-27 21:04:30 +0200

    small refactor

--- app/utils.py
+++ app/utils.py
@@ -18,36 +18,50 @@ class Finder:
         self._raw_content: bs | None = None
         self._data_current_run: list | None = None
 
+    @property
+    def data_current_run(self) -> list | None:
+        """Getter of attribute."""
+        return self._data_current_run
+
     def get_web_content(self) -> None:
         """Send GET request to website and fetch its content."""
-        response: bytes = _send_http_get_request(self._url)
-        self._raw_content = bs(response, "html.parser")
+        session: requests.Session = requests.Session()
+        retries: Retry = Retry(total=3, backoff_factor=1)
+        session.mount("https://", HTTPAdapter(max_retries=retries))
+        response: requests.Response = session.get(
+            url=self._url,
+            timeout=10,
+            headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+            },
+        )
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            raise
+        self._raw_content = bs(response.content, "html.parser")
 
     def parse_content_to_dataframe(self) -> None:
         """Retrieve relevant data from raw content and parse it to dataframe."""
         data: list = []
-        list_of_items: list = (
+        items: list = (
             self._raw_content.find_all("div", class_=WEB_CLASSES["olx_items"])
-            if self._raw_content
-            else []
         )
-        for item in list_of_items:
+        for item in items:
             id_text: str | None = item.get("id")
-
             if not id_text:
                 continue
 
             url_tag = item.find("a", class_=WEB_CLASSES["olx_item_url"])
             header_tag = item.find("h6", class_=WEB_CLASSES["olx_item_header"])
             price_tag = item.find("p", class_=WEB_CLASSES["olx_item_price"])
-            refresh_date_tag = item.find(
-                "p", class_=WEB_CLASSES["olx_item_refresh_date"]
-            )
+            refresh_dt_tag = item.find("p", class_=WEB_CLASSES["olx_item_refresh_dt"])
 
             url_text: str = url_tag["href"] if url_tag else "NA"
             header_text: str = header_tag.text if header_tag else "NA"
             price_text: str = price_tag.text if header_tag else "NA"
-            refresh_date_text: str = refresh_date_tag.text if refresh_date_tag else "NA"
+            refresh_dt_text: str = refresh_dt_tag.text if refresh_dt_tag else "NA"
 
             if url_text.startswith("/d/"):
                 url_text = "https://www.olx.pl" + url_text
@@ -58,34 +72,10 @@ class Finder:
                     "url": url_text,
                     "header": header_text,
                     "price": price_text,
-                    "refresh_date": refresh_date_text,
+                    "refresh_date": refresh_dt_text,
                 }
             )
         self._data_current_run = data
 
     def send_notification_to_users(self) -> None:
         """Send email notification to defined users."""
-
-    @property
-    def data_current_run(self) -> list | None:
-        """Getter of attribute."""
-        return self._data_current_run
-
-
-def _send_http_get_request(url: str) -> bytes:
-    session: requests.Session = requests.Session()
-    retries: Retry = Retry(total=3, backoff_factor=1)
-    session.mount("https://", HTTPAdapter(max_retries=retries))
-    response: requests.Response = session.get(
-        url=url,
-        timeout=10,
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
-        },
-    )
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print("Failed to fetch.", err)  # TODO LOGGER
-        raise
-    return response.content
