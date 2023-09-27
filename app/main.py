7097d8d5a9a2ebb187deebc16a069e62230c1dac
Norbert <norbert.lipinski2@sky.uk>
2023-09-27 15:01:59 +0100

    Parsing from raw to dict

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
+from app.config import WEB_URL
 from app.utils import Finder
-from app.config import web_url
+
 
 def run() -> None:
-    '''Main function.'''
-    finder = Finder(web_url)
-    finder.send_http_request()
-    print(finder.response)
-if __name__ == '__main__':
+    """Main function."""
+    finder = Finder(WEB_URL)
+    finder.get_web_content()
+    finder.parse_content_to_dataframe()
+
+
+if __name__ == "__main__":
     run()
--- app/utils.py
+++ app/utils.py
@@ -1,26 +1,91 @@
-from bs4 import BeautifulSoup as bs
 import pendulum
-from loguru import logger
-import requests
 import polars as pl
-from app.config import olx_items
+import requests
+from bs4 import BeautifulSoup as bs
+from loguru import logger
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+
+from app.config import WEB_CLASSES
+
+
 class Finder:
+    """Finder class."""
+
     def __init__(self, url: str) -> None:
-        self._url = url
-        self._response = None
+        """Init."""
+        self._url: str = url
+        self._raw_content: bs | None = None
+        self._data_current_run: list | None = None
 
-    def send_http_request(self) -> None:
-        response = requests.get(self._url)
-        soup = bs(response.content, 'html.parser')
-        data = soup.find_all('div', class_=olx_items)
-        self._response = data
+    def get_web_content(self) -> None:
+        """Send GET request to website and fetch its content."""
+        response: bytes = _send_http_get_request(self._url)
+        self._raw_content = bs(response, "html.parser")
 
-    def parse_content_to_dataframe(self) -> ...:
-        ...
+    def parse_content_to_dataframe(self) -> None:
+        """Retrieve relevant data from raw content and parse it to dataframe."""
+        data: list = []
+        list_of_items: list = (
+            self._raw_content.find_all("div", class_=WEB_CLASSES["olx_items"])
+            if self._raw_content
+            else []
+        )
+        for item in list_of_items:
+            id_text: str | None = item.get("id")
 
-    def send_notification_to_users(self) -> ...:
-        ...
+            if not id_text:
+                continue
+
+            url_tag = item.find("a", class_=WEB_CLASSES["olx_item_url"])
+            header_tag = item.find("h6", class_=WEB_CLASSES["olx_item_header"])
+            price_tag = item.find("p", class_=WEB_CLASSES["olx_item_price"])
+            refresh_date_tag = item.find(
+                "p", class_=WEB_CLASSES["olx_item_refresh_date"]
+            )
+
+            url_text: str = url_tag["href"] if url_tag else "NA"
+            header_text: str = header_tag.text if header_tag else "NA"
+            price_text: str = price_tag.text if header_tag else "NA"
+            refresh_date_text: str = refresh_date_tag.text if refresh_date_tag else "NA"
+
+            if url_text.startswith("/d/"):
+                url_text = "https://www.olx.pl" + url_text
+
+            data.append(
+                {
+                    "id": id_text,
+                    "url": url_text,
+                    "header": header_text,
+                    "price": price_text,
+                    "refresh_date": refresh_date_text,
+                }
+            )
+        self._data_current_run = data
+
+    def send_notification_to_users(self) -> None:
+        """Send email notification to defined users."""
 
     @property
-    def response(self) -> str:
-        return self._response
+    def data_current_run(self) -> list | None:
+        """Getter of attribute."""
+        return self._data_current_run
+
+
+def _send_http_get_request(url: str) -> bytes:
+    session: requests.Session = requests.Session()
+    retries: Retry = Retry(total=3, backoff_factor=1)
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+    response: requests.Response = session.get(
+        url=url,
+        timeout=10,
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
+        },
+    )
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print("Failed to fetch.", err)  # TODO LOGGER
+        raise
+    return response.content
