271ea6df5673c1788510f01eb7e3ddcf92e25a44
Norbert <norbert.lipinski2@sky.uk>
2023-10-19 14:56:14 +0100

    v2.0 Refactored

--- app/config.py
+++ app/config.py
@@ -1,24 +1,23 @@
 import os
 
-WEB_URL: str = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/wroclaw/?page=&search%5Bfilter_enum_floor_select%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_select%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_select%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_select%5D%5B3%5D=floor_4&search%5Bfilter_float_m%3Afrom%5D=40&search%5Border%5D=created_at%3Adesc&view=list"
+SENDER = {
+    "email": os.getenv("sender_email", ""),
+    "password": os.getenv("sender_pwd", ""),
+}
+RECIPIENTS = [
+    # "bartoszek.jus@gmail.com",
+    "trebronszef1@gmail.com",
+]
 
-WEB_CLASSES: dict[str, str] = {
+FLAT_WEB_URL = "https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/wroclaw/?page=&search%5Bfilter_enum_floor_select%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_select%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_select%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_select%5D%5B3%5D=floor_4&search%5Bfilter_float_m%3Afrom%5D=40&search%5Border%5D=created_at%3Adesc&view=list"
+FLAT_WEB_CLASSES = {
     "olx_items": "css-1sw7q4x",
     "olx_item_url": "css-rc5s2u",
     "olx_item_header": "css-16v5mdi",
     "olx_item_price": "css-10b0gli",
     "olx_item_refresh_dt": "css-veheph",
 }
-
-REQUEST_HEADERS: dict[str, str] = {
+FLAT_REQUEST_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
 }
-
-PATH_ITEMS: str = "data/items.csv"
-
-SENDER_EMAIL: str = os.getenv("sender_email", "")
-SENDER_PWD: str = os.getenv("sender_pwd", "")
-SUBSCRIBERS: list[str] = [
-    # "bartoszek.jus@gmail.com",
-    "trebronszef1@gmail.com",
-]
+FLAT_DB_PATH = "data/flats.csv"
--- app/main.py
+++ app/main.py
@@ -1,12 +1,38 @@
-from app.utils import Finder
+import app.config as cfg
+from app.flats import FlatNotifier, FlatParser, FlatScraper
 
 
 def run() -> None:
     """Main function."""
-    finder = Finder()
-    finder.get_web_content()
-    finder.parse_content_to_dataframe()
-    finder.send_notification_to_users()
+    flat_scraper = FlatScraper()
+    flat_scraper.url = cfg.FLAT_WEB_URL
+    flat_scraper.headers = cfg.FLAT_REQUEST_HEADERS
+    flat_scraper.scrap_webpage()
+
+    if flat_scraper.error_occured:
+        return 0
+
+    flat_parser = FlatParser()
+    flat_parser.load_dataframe_from_database(db_path=cfg.FLAT_DB_PATH)
+    flat_parser.content_raw = flat_scraper.content
+    flat_parser.classes = cfg.FLAT_WEB_CLASSES
+    flat_parser.parse_raw_content_to_dataframe()
+
+    if flat_parser.error_occured:
+        return 0
+
+    flat_notifier = FlatNotifier()
+    flat_notifier.dataframe = flat_parser.content_dataframe
+    flat_notifier.recipients = cfg.RECIPIENTS
+    flat_notifier.sender = cfg.SENDER
+    flat_notifier.send_notification()
+
+    if flat_notifier.error_occured:
+        return 0
+
+    flat_parser.save_dataframe_in_database(db_path=cfg.FLAT_DB_PATH)
+
+    return 1
 
 
 if __name__ == "__main__":
